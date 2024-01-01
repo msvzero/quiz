d845fc8955900fd272eaee29b116056440581153
msvzero2021 <msvzero2021@gmail.com>
2024-01-01 17:53:00 -0300

    Actualizado pantalla de inicio

--- lib/custom_widget/start_screen.dart
+++ lib/custom_widget/start_screen.dart
@@ -5,6 +5,27 @@ class StartScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text('Start Screen');
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+          ),
+          const SizedBox(height: 30), // new line
+          const Text(
+            'Learn Flutter with us!',
+            style: TextStyle(color: Colors.white, fontSize: 30),
+          ),
+          const SizedBox(height: 30), // new line
+          OutlinedButton(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
+            child: const Text('Start Quiz'),
+          )
+        ],
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:quiz_app/custom_widget/start_screen.dart';
 
 void main() {
-  runApp(const MaterialApp(
+  runApp(
+    MaterialApp(
       title: 'My app', // used by the OS task switcher
       home: Scaffold(
-        body: Center(
-          child: StartScreen(),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Colors.deepPurple,
+                Colors.purple,
+              ],
+            ),
+          ),
+          child: const StartScreen(),
         ),
-      )));
+      ),
+    ),
+  );
 }
