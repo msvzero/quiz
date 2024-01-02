3a016d7c067a4811c827ba2c770a39927a28a452
msvzero2021 <msvzero2021@gmail.com>
2024-01-02 18:22:23 -0300

    Actualizado pantalla para mostrar preguntas y opciones

---
+++ lib/custom_widget/answerButton.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class AnswerButton extends StatelessWidget {
+  const AnswerButton(this.answerText, this.onTap, {super.key});
+
+  final String answerText;
+  final void Function() onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onTap,
+      style: ElevatedButton.styleFrom(
+          padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+          backgroundColor: const Color.fromARGB(255, 33, 1, 95),
+          foregroundColor: Colors.white,
+          shape: const LinearBorder()),
+      child: Text(answerText),
+    );
+  }
+}
--- lib/custom_widget/question_screen.dart
+++ lib/custom_widget/question_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/custom_widget/answerButton.dart';
+import 'package:quiz_app/data/question.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -10,8 +12,32 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
+  final currentQuestion = questions[0];
+
   @override
   Widget build(BuildContext context) {
-    return Center(child: const Text('Questions Screen'));
+    return Container(
+      margin: const EdgeInsets.all(40),
+      child: SizedBox(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: const TextStyle(color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 30),
+            ...currentQuestion.answers.map(
+              (answer) => Container(
+                  margin: const EdgeInsets.all(10),
+                  child: AnswerButton(answer, () {})),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
